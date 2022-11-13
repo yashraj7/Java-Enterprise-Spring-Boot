d16aae1b20f5714772d84a755ba2f86e3ec83af1
yashraj7 <43900397+yashraj7@users.noreply.github.com>
2022-11-14 01:08:33 +0530

    Add files via upload

---
+++ Core_Java/Assignment 3 String/_3Pallindrome.java
@@ -0,0 +1,14 @@
+public class _3Pallindrome {
+    public static boolean checkPallindrome(String s, int i, int j) {
+        while (i < j) {
+            if (s.charAt(i++) != s.charAt(j--))
+                return false;
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        String s = "2552";//Output : true
+        int n = s.length();
+        System.out.println(checkPallindrome(s.toLowerCase(), 0, n - 1));       
+    }
+}
